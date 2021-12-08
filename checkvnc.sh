e63c525dfa71556a3c26945df3fcdb4937fcc01b
Austin Leath <austinleath18@gmail.com>
2021-12-07 21:26:33 -0600

    add checkvncuse function add recursive calls

--- checkvnc.sh
+++ checkvnc.sh
@@ -1,24 +1,32 @@
-#inuse=`lsof -PiTCP | grep ":5900"`; was going to use this, but it was sometimes innacurate
+#!/bin/bash
 inuse=`netstat -vanp tcp | grep 5900 | grep ESTABLISHED`
 
-if [ -n "$inuse" ]
+function checkvncuse {
+  if [ -n "$inuse" ]
     then
-        echo "in use, exiting...";
-        #do not do anything because the computer is currently in use
-        exit;
+        #VNC in use. return 1
+        return 1;
     else
-        echo "not in use";
-        while [ True ]
-        do
-            variable="$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))"; #get seconds that user has been idle for
-            sleep 1; #wait one second
-            if [ "$variable" -gt "30" ]
-                then
-                    echo "idle time greater than 30 seconds, signing out user and exiting...";
-                    #do things like sign out a user after 30 seconds of no vnc connection and 30 seconds of inactivity
-                    exit;
-            fi
-        done
-fi
+        #VNC not in use. return 0
+        return 0;
+  fi
+}
 
-
+while [ True ]
+do
+    idletime="$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))"; #get seconds that user has been idle for
+    if checkvncuse "$notinuse"; 
+        then
+        if [ "$idletime" -gt "30" ]; 
+            then
+                echo "VNC not in use, idle time greater than 30 seconds, signing out user and exiting...";
+                #do things like sign out a user after 30 seconds of no vnc connection and 30 seconds of inactivity
+                exit;
+        else
+            echo "VNC not in use, waiting for idle time to be greater than 30 seconds... idle time: $idletime";
+        fi
+    else
+        echo "VNC in use, doing nothing.";
+    fi
+    sleep 1; #wait one second
+done
